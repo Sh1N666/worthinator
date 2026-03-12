e1a5772c0fb7bba0fabdff34d75aa77e992e6f57
milosz <m.jacoszek01@gmail.com>
2026-03-12 23:22:08 +0100

    add search api

---
+++ src/ApiInator/Application/SteamApi/SteamApiConfigure.cs
@@ -0,0 +1,9 @@
+namespace ApiInator.Application.SteamApi;
+
+public static class SteamApiConfigure
+{
+    public static void AddSteamApi(this IServiceCollection services)
+    {
+        services.AddScoped<SteamApi>();
+    }
+}
--- src/ApiInator/Program.cs
+++ src/ApiInator/Program.cs
@@ -1,20 +1,36 @@
-using ApiInator.Infrastructure;
+using ApiInator.Application;
+using ApiInator.Application.SteamApi;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
+using SlimMessageBus.Host;
+using SlimMessageBus.Host.Memory;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddSlimMessageBus(mbb =>
+{
+    mbb
+        .WithProviderMemory()
+        .AutoDeclareFrom(typeof(Program).Assembly); 
+});
+builder.Services.AddGrpc();
+builder.Services.AddSteamApi();
+
 var app = builder.Build();
 
-app.MapGet("/status", () => "I'm Alive");
+app.MapGet("/status", ([FromServices]SteamApi steamApi) => "I'm Alive");
+
+app.MapGrpcService<ApiInatorController>();
 
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
-    db.Database.Migrate();
+    //db.Database.Migrate();
 }
 
 app.Run();
+
